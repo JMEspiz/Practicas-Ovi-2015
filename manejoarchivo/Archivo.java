02846cf5db1960fe461e2cb76f31ff9b1bfa5281
JMEspiz <josemepiz@gmail.com>
2015-05-14 00:37:34 -0430

    pequeños cambios

--- manejoarchivo/Archivo.java
+++ manejoarchivo/Archivo.java
@@ -1,29 +1,30 @@
 package manejoarchivo;
 
 
-        /**********************************************************/
-		/* Convenciones para nombre de Variables, Metodos y Clases*/
-		/* VARAIBLES: nombres en minusculas y separados con '_'.  */
-		/*            Ej: nombre_archivo;                         */
-		/* METODOS: camelCase, primera letra en miniscula y luego */
-		/*          mayuscula. Ej:  abrirArchivo();               */
-		/*CLASES: Mayusculas primera letra de cada palabra, EJ:   */
-		/*        ArchivoBase {} o Archivo{}                      */
-		/*INSTANCIAS: primera letra de la clase. Ej: instancia    */
-		/*            la clase -> Archivo a = new Archivo()       */
-		/*                                                        */
-		/* Son solo sugerencias para un buen trabajo en equipo    */
-		/* Abierto a cambios por decisión grupal                  */
-		/**********************************************************/
+/**********************************************************/
+/* Convenciones para nombre de Variables, Metodos y Clases*/
+/* VARAIBLES: nombres en minusculas y separados con '_'.  */
+/*            Ej: nombre_archivo;                         */
+/* METODOS: camelCase, primera letra en miniscula y luego */
+/*          mayuscula. Ej:  abrirArchivo();               */
+/*CLASES: Mayusculas primera letra de cada palabra, EJ:   */
+/*        ArchivoBase {} o Archivo{}                      */
+/*INSTANCIAS: primera letra de la clase. Ej: instancia    */
+/*            la clase -> Archivo a = new Archivo()       */
+/*                                                        */
+/* Son solo sugerencias para un buen trabajo en equipo    */
+/* Abierto a cambios por decisión grupal                  */
+/**********************************************************/
 
-		/**********************************************************/
-		/* OBJETIVOS:                                             */
-		/* 		Crear una clase base con el objeto para manejar   */
-		/*   Archivos: Crear, Leer, Escribir, Borrar, Buscar      */
-		/**********************************************************/
+/**********************************************************/
+/* OBJETIVOS:                                             */
+/*	Crear una clase base con el objeto para manejar       */
+/*   Archivos: Crear, Leer, Escribir, Borrar, Buscar      */
+/**********************************************************/
 
 
-import java.io.*;
+//import java.io.*;
+
 
 class Archivo {
 
@@ -31,9 +32,11 @@ class Archivo {
 		           
 		//VARIABLES:
 
-		String nombre_archivo, ruta_archivo, ruta_completa;
+		public String nombre_archivo;
+		public String ruta_archivo;
+		public String ruta_completa;
 
-		Archivo(){
+		public Archivo(){
 
 			this.nombre_archivo = "ejemplo.txt";
 			this.ruta_archivo ="";
@@ -41,10 +44,16 @@ class Archivo {
 
 		}
 
+		public Archivo(String nombre, String ruta){
 
-		public void getRuta(String ruta_completa){
-
-			System.out.println(this.ruta_completa);
+			this.nombre_archivo = nombre;
+			this.ruta_archivo =ruta;
+			this.ruta_completa = ruta + nombre;
 		}
-}
 
+
+		public String getRuta(String ruta_completa){
+
+			return ruta_completa;
+		}
+	}
--- manejoarchivo/Main.java
+++ manejoarchivo/Main.java
@@ -1,35 +1,37 @@
 package manejoarchivo;
 
 
-        /**********************************************************/
-		/* Convenciones para nombre de Variables, Metodos y Clases*/
-		/* VARAIBLES: nombres en minusculas y separados con '_'.  */
-		/*            Ej: nombre_archivo;                         */
-		/* METODOS: camelCase, primera letra en miniscula y luego */
-		/*          mayuscula. Ej:  abrirArchivo();               */
-		/*CLASES: Mayusculas primera letra de cada palabra, EJ:   */
-		/*        ArchivoBase {} o Archivo{}                      */
-		/*INSTANCIAS: primera letra de la clase. Ej: instancia    */
-		/*            la clase -> Archivo a = new Archivo()       */
-		/*                                                        */
-		/* Son solo sugerencias para un buen trabajo en equipo    */
-		/* Abierto a cambios por decisión grupal                  */
-		/**********************************************************/
 
-		/**********************************************************/
-		/* OBJETIVOS:                                             */
-		/* 		Crear una clase base con el objeto para manejar   */
-		/*   Archivos: Crear, Leer, Escribir, Borrar, Buscar      */
-		/**********************************************************/
+/**********************************************************/
+/* Convenciones para nombre de Variables, Metodos y Clases*/
+/* VARAIBLES: nombres en minusculas y separados con '_'.  */
+/*            Ej: nombre_archivo;                         */
+/* METODOS: camelCase, primera letra en miniscula y luego */
+/*          mayuscula. Ej:  abrirArchivo();               */
+/*CLASES: Mayusculas primera letra de cada palabra, EJ:   */
+/*        ArchivoBase {} o Archivo{}                      */
+/*INSTANCIAS: primera letra de la clase. Ej: instancia    */
+/*            la clase -> Archivo a = new Archivo()       */
+/*                                                        */
+/* Son solo sugerencias para un buen trabajo en equipo    */
+/* Abierto a cambios por decisión grupal                  */
+/**********************************************************/
 
+/**********************************************************/
+/* OBJETIVOS:                                             */
+/*	Crear una clase base con el objeto para manejar       */
+/*   Archivos: Crear, Leer, Escribir, Borrar, Buscar      */
+/**********************************************************/
 
-import java.io.*;
+//import java.io.*;
+
 
 class Main {
 
 	public static void main(String[] args){
 
-		Archivo a = new Archivo();
+		Archivo a;
+		a = new Archivo("hola.txt", "/home/jmex/");
 
 		a.getRuta();
 
