65366db83b56c7e7d80cc5a7ad5ca692e9e601de
yormi <yormian@gmail.com>
2015-05-15 23:10:27 -0430

    he agregado nuevas funciones

--- LeerArchivo/EjercicioArchivo.java
+++ LeerArchivo/EjercicioArchivo.java
@@ -3,50 +3,79 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StreamTokenizer;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.io.File;
 
+//import java.io*; con el asterisco no es necesario instaciamos libreria por lbreria 
 public class EjercicioArchivo {
 
-   public static void mostarContenido (String archivo){
-    String cadena;
-   try{
-    FileReader f = new FileReader(archivo);
-    BufferedReader b = new BufferedReader(f);
+   public static void LeerArchivo (String archivo){
+     // String cadena;
+       try{
+          FileReader f = new FileReader(archivo);
+          BufferedReader b = new BufferedReader(f);
+          String cadena = "";//la de claro vacia 
 
-      while((cadena = b.readLine())!=null){
-          System.out.println(cadena);
-         }
-             b.close();
-            }
-            catch(FileNotFoundException error){
-              System.out.println("No se Encotron el Archivo");
-            }
-             catch(IOException error){
+         while(cadena!=null){
+           cadena = b.readLine();
+          /*con este if y el break se controla que si el documento 
+          .txt esta vacio no retorne la palabra null en Pantalla */
+              if (cadena==null) 
+                   break;
+               System.out.println(cadena);
+           }
+           b.close();
+            
+          }catch(IOException error){
               System.out.println("Se produjo un error de E/S");
             }
+
     }//Fin Del Metodo mostrarContenido
-         /*con este metodo cuneto el numero de palabras */
-    public static void contarPalabras(String archivo){
+
+     /*Metodo que me permite Contar en numero de palabras de un texto */
+     public static void contarPalabras(String archivo){
        int palabras = 0;
              try{
           /* se tiene que manejar Excepciones 
            por que se esta trabajndo con un fichero */
-            FileReader f = new FileReader(archivo);//este recibe el Archivo .txt
-          //  BufferedReader b = new BufferedReader(f);
-            StreamTokenizer st = new StreamTokenizer(f); //le paso la instancia f
-          while(st.nextToken() != StreamTokenizer.TT_EOF){
+
+         FileReader f = new FileReader(archivo);//este recibe el Archivo .txt
+         StreamTokenizer st = new StreamTokenizer(f); //le paso la instancia f
+          
+            while(st.nextToken() != StreamTokenizer.TT_EOF){
                  if(st.ttype == StreamTokenizer.TT_WORD){
                       palabras++;
                  }
+
                }//fin del While
+               
                System.out.println("Cantidad DE Palabras "+palabras);
              }//findel try
+
              catch(IOException error){
                 System.out.println("Se produjo un error de E/S");
-          }      
+          } 
+     
      }//fin del metodo contarPalabras
 
-     public static void main(String[] args){
-     mostarContenido("/home/yormi/Practicas-Ovi-2015/LeerArchivo/Archivo.txt");
-     contarPalabras("/home/yormi/Practicas-Ovi-2015/LeerArchivo/Archivo.txt");
- }//Fin del Main
+     /*Metodo que me permite escribir un en u  texto .txt*/
+     public static void escribirTexto(String nombreArchivo){
+          File f = new File(nombreArchivo);
+          try{
+              FileWriter w = new FileWriter(f);
+              BufferedWriter bw = new BufferedWriter(w);
+              PrintWriter wr = new PrintWriter(bw);
+              
+              wr.write("Como Escribir un archivo plano .txt");
+              wr.append("\nParticando para en la OVI 2015");
+              wr.close();
+              bw.close();
+              
+             }catch(IOException error){
+                 System.out.println("Error");
+          }
+     }//fin del mentodo escribirTexto
+
 }//Fin de la Clase
